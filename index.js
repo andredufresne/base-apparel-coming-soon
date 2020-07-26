ab1eea1e3618de38d0e569357a20c3a44b7565b8
Andre Dufresne <a_dufresne@coloradocollege.edu>
2020-07-26 13:51:38 -0600

    no longer allows value entry

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getUserEmail() {
     var emailField = document.getElementById('email').value;
     var result = document.getElementById('result').value;
 
-    if (emailField === null || emailField === "") {
+    if (emailField === null || emailField === "" || emailField === "Email Address") {
         alert('Login cannot be empty')     
     } 
 }

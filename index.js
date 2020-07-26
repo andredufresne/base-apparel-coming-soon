3cbbfb7c6ad99689b7f520796d7b5797f0a9e82a
Andre Dufresne <a_dufresne@coloradocollege.edu>
2020-07-26 13:25:05 -0600

    Added functionality and styling

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-prompt("What is your name?")
+function getUserEmail() {
+    var emailField = document.getElementById('email').value;
+    var result = document.getElementById('result').value;
+
+    if (emailField === null || emailField === "") {
+        alert('Login cannot be empty')     
+    } 
+}
+
+var subButton = document.getElementById("button");
+subButton.addEventListener('click', getUserEmail, false);
+
